f9547161822c3d9f5946d3a05f94a97221533650
shafira khuzaimi <firakhuzaimisaibah@gmail.com>
2026-03-04 16:31:06 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#14b8a6', // Ganti dengan kode HEX warna pilihanmu
+        
+      },
+    },
+  },
+  plugins: [],
+}
